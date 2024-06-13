2dd1e5e740728f83ed101781c3a11ad470a68ceb
snoluv45 <pasitza115@gmail.com>
2024-06-14 04:13:19 +0700

    65%

--- index.php
+++ index.php
@@ -13,6 +13,10 @@ require_once('LineLogin.php');
     <title>Line Login</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/medic.css">
+    <link rel="stylesheet" type="text/css" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
     <style>
     body {
       background-image: url('assets/images/bluewhite.jpg');
