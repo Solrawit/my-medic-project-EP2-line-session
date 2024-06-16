4d4e6efa3652fe6a8bbba66b30697f7725bdcbf7
snoluv45 <pasitza115@gmail.com>
2024-06-16 17:18:39 +0700

    Update nav_user.php

--- component/nav_user.php
+++ component/nav_user.php
@@ -46,10 +46,10 @@ require_once('./LineLogin.php');
           <a class="nav-link" href="page_user/text_photo.php"><i class="fa fa-language fa-lg"></i> OCR V.2 <span class="sr-only"></span></a>
             </li>
             <li class="nav-item">
-          <a class="nav-link" href="./alert_time.php"><i class="fa fa-clock-o fa-lg"></i> ตั้งค่าการแจ้งเตือน <span class="sr-only"></span></a>
+          <a class="nav-link" href="page_user/alert_time.php"><i class="fa fa-clock-o fa-lg"></i> ตั้งค่าการแจ้งเตือน <span class="sr-only"></span></a>
             </li>
             <li class="nav-item">
-          <a class="nav-link" href="./alert_time.php"><i class="fa fa-history"></i> รายการของฉัน <span class="sr-only"></span></a>
+          <a class="nav-link" href="page_user/alert_time.php"><i class="fa fa-history"></i> รายการของฉัน <span class="sr-only"></span></a>
             </li>
             <li class="nav-item">
           <a class="nav-link" href="./welcome.php"><i class="fa fa-youtube-play fa-lg"></i> วิธีการใช้งาน <span class="sr-only"></span></a>
