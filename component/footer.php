2f41d8fe73c7bf75f4282b532542d5cba372d107
snoluv45 <pasitza115@gmail.com>
2024-06-19 06:08:28 +0700

    fewfew

--- component/footer.php
+++ component/footer.php
@@ -1,5 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Title</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    footer {
+  color: #ffffff; /* White text color */
+  padding: 20px 0; /* Padding for top and bottom */
+  margin-top: 20px; /* Space above the footer */
+  text-align: center; /* Center align text */
+}
+
+footer a {
+  color: #000000; /* White color for links */
+}
+
+footer a:hover {
+  text-decoration: none; /* Remove underline on hover */
+}
+
+  </style>
 <hr>
 <footer>
-  <p class="text-center text-secondary">Medic Project 2024 - Developed by <a href="" class="footer" target="_blank">PROJECT DEV UNIVERSITY</a></p>
-  <p class="text-center text-secondary">  © Copyright 2024 Website By Computer Science RMUT All Rights Reserved. ติดต่อเจ้าของร้านไม่ได้ คลิก !! <a href="" class="footer" target="_blank">BETA TEST</a></p>
+  <p class="text-center text-dark">Medic Project 2024 - Developed by <a href="" class="footer" target="_blank">PROJECT DEV UNIVERSITY</a></p>
+  <p class="text-center text-dark">  © Copyright 2024 Website By Computer Science RMUT All Rights Reserved. ติดต่อเจ้าของร้านไม่ได้ คลิก !! <a href="" class="footer" target="_blank">BETA TEST</a></p>
 </footer>
--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if (isset($_SESSION['profile'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Line Login</title>
+    <title>หน้าหลัก</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
@@ -76,7 +76,7 @@ if (isset($_SESSION['profile'])) {
             animation: moveText 3s infinite;
         }
       body {
-        background-image: url('assets/images/bluewhite.jpg');
+        background-image: url('assets/images/wpp2.jpg');
         background-size: cover;
         background-position: center;
         animation: fadeIn 2s ease-in-out;
