e79295bf8372fcb17c6b21982a3b22ac01049c22
snoluv45 <pasitza115@gmail.com>
2024-07-02 06:33:15 +0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,6 +1,10 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost;dbname=mdpj_user;charset=utf8', 'root', '');
+    $db = new PDO(
+        'mysql:host=localhost;dbname=mdpj_user;charset=utf8', 
+        'root', 
+        ''
+    );
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo 'Error: ' . $e->getMessage();
