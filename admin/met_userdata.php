d75a1ba8b0d4296a33535808b9b69f3ce73f00a9
snoluv45 <pasitza115@gmail.com>
2024-07-29 05:49:29 +0700

    Update met_userdata.php

--- admin/met_userdata.php
+++ admin/met_userdata.php
@@ -42,6 +42,7 @@ $users = $stmt->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Data</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="../favicon.png">
     <style>
         body {
             position: relative;
