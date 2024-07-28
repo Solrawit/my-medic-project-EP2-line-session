6143de50f0374e08f5003a7c4c21bf96e09f585c
snoluv45 <pasitza115@gmail.com>
2024-07-29 05:38:53 +0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -95,7 +95,7 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MEDICINE 1.0</title>
+    <title>MEDICINE.NET</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/forwelcome.css">
