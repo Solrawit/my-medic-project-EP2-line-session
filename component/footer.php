2667ed8a1b1585b8ab080c6d4af4b98e06fd9d38
snoluv45 <pasitza115@gmail.com>
2024-08-17 19:44:22 +0700

    Update footer.php

--- component/footer.php
+++ component/footer.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Title</title>
-  <!-- Bootstrap CSS -->
+  <!-- CDN Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Font Awesome CSS -->
+  <!-- CDN Font Awesome CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <style>
