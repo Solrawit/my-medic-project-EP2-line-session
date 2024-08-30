bafd987464da237f67661ec115849f446ed394b2
s_noluv45 <pasitza115@gmail.com>
2024-08-31 05:21:58 +0700

    Update text_photo.php

--- page_user/text_photo.php
+++ page_user/text_photo.php
@@ -270,6 +270,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/tesseract-js@0.0.1/lib/tesseract.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/easy-tesseract-ocr@1.2.2/index.min.js"></script>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -491,7 +494,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
         </div>
     </div>
 </div>
-
+<script src="https://cdn.jsdelivr.net/npm/tesseract-js@0.0.1/lib/tesseract.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/easy-tesseract-ocr@1.2.2/index.min.js"></script>
 <script>
 document.addEventListener("DOMContentLoaded", function() {
     $('#startOcrButton').click(function() {
